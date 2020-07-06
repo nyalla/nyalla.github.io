a921dc95936aa527b0a71df56064e12973f6cbde
Naveen Yalla <naveenyalla22@gmail.com>
2020-07-06 13:45:33 +0530

    Modified success message
    Modified success message

--- js/contact_me.js
+++ js/contact_me.js
@@ -36,7 +36,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+           .append("<strong>"+"Hello  " + firstName + ", I will get back to you shortly. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
